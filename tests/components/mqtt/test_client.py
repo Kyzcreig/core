92171dcce58bcfdc9fec5b0cb594ad3db549b2a1
jbouwh <jan@jbsoft.nl>
2026-05-13 22:11:10 +0000

    Fix CI test

--- tests/components/mqtt/test_client.py
+++ tests/components/mqtt/test_client.py
@@ -1379,12 +1379,13 @@ async def test_publish_error(
 
 async def test_subscribe_error(
     hass: HomeAssistant,
-    setup_with_birth_msg_client_mock: MqttMockPahoClient,
+    mqtt_mock_entry: MqttMockHAClientGenerator,
+    mqtt_client_mock: MqttMockPahoClient,
     record_calls: MessageCallbackType,
     caplog: pytest.LogCaptureFixture,
 ) -> None:
     """Test publish error."""
-    mqtt_client_mock = setup_with_birth_msg_client_mock
+    await mqtt_mock_entry()
     mqtt_client_mock.reset_mock()
     # simulate client is not connected error before subscribing
     mqtt_client_mock.subscribe.side_effect = lambda *args: (4, None)
@@ -1392,7 +1393,9 @@ async def test_subscribe_error(
     while mqtt_client_mock.subscribe.call_count == 0:
         await hass.async_block_till_done()
     await hass.async_block_till_done()
-    assert "mqtt_broker_error" in caplog.text
+    assert (
+        "Error talking to MQTT: The client is not currently connected." in caplog.text
+    )
 
 
 async def test_handle_message_callback(
